5e88a175ce3ced09ba7d4fc0e0cbf17411c40140
gaganthapar12309 <gsthapar@gmail.com>
2022-10-26 17:37:42 -0700

    Continuing lit review; need to update figure for sensors and star trackers

---
+++ figures/figureGen.m
@@ -0,0 +1,80 @@
+%% Gagandeep Thapar Figure Generation
+clc;
+clear all;
+close all;
+%% Literature Review
+
+% Cost v Performance of Sensors
+sz = 100;
+
+% star trackers
+% var = {cost, cross-boresight, roll-boresight, name}
+rocketlab = {140000, 5, 55, 'Rocket Lab ST-16RT2'};
+maryland = {32000, 0.1*3600, 0.1*3600*6, 'Adcole Maryland Aerospace'};
+twinkle = {45000, 15, 90, 'arcsec Twinkle'};
+sagitta = {45000, 2, 10, 'arcsec Sagitta'};
+
+sodern = {445000, 5.2, 32.6, 'Sodern HYDRA'};
+jena = {400000, 2.78, 28, 'Jena A-APS'};
+leo = {709000, 3.57, 23.6, 'Leonardo AA-STR'};  
+ball = {1000000, 1.5, 1.5, 'Ball CT2020'};
+micro = {441000, 1, 3, 'Space Micro \microStar-200M'};
+
+startrackers = [rocketlab;
+                maryland;
+                twinkle;
+                sagitta;
+                sodern;
+                jena;
+                leo;
+                ball;
+                micro];
+
+% sun sensors
+bison = {9000, 2*3600, 'BiSon64-ET'};
+ssoc = {4000, 0.5*3600, 'nanoSSOC-D60'};
+
+% magnetometers
+
+
+[a, ~] = size(startrackers);
+names = cell(1, a);
+
+figure
+hold on
+
+scatter(rocketlab{1}/1000, rocketlab{2}, sz, 'pentagram', 'red', 'filled')
+scatter(bison{1}/1000, bison{2}, sz, 'diamond', 'blue', 'filled')
+scatter(ssoc{1}/1000, ssoc{2}, sz, 'diamond', 'blue', 'filled')
+
+for i = 2:a
+    sensor = startrackers(i,:);
+    scatter(sensor{1}/1000, sensor{2}, sz, 'pentagram', 'red', 'filled');
+
+    names{1, i} = sensor{4};
+end
+hold off
+
+title('Cost VS Performance: Cross-Boresight')
+xlabel('Cost [in Thousands of $]')
+ylabel('Cross Boresight [arcsec]')
+set(gca,'yscale','log')
+grid on
+axis padded
+legend('Star Tracker', 'Sun Sensor')
+
+figure
+hold on
+for i = 1:a
+    sensor = startrackers(i,:);
+    scatter(sensor{1}/1000, sensor{2}, sz, 'pentagram', 'red', 'filled');
+
+    names{1, i} = sensor{4};
+end
+hold off
+set(gca,'yscale','log')
+title('Cost VS Performance: Cross-Boresight')
+xlabel('Cost [in Thousands of $]')
+ylabel('Cross Boresight [arcsec]')
+grid on
+axis padded
