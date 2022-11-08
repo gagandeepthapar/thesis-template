4fc4b2180960c5697d9659100993656ef1bb5c40
gaganthapar12309 <gsthapar@gmail.com>
2022-11-07 17:44:58 -0800

    Finished initial draft of lit review

--- figures/figureGen.m
+++ figures/figureGen.m
@@ -25,11 +25,14 @@ startrackers = [rocketlab;
                 berlin];
 
 % sun sensors
-bison = {9000, 2*3600, 'BiSon64-ET'};
+% bison = {9000, 2*3600, 'BiSon64-ET'};
 ssoc = {4000, 0.5*3600, 'nanoSSOC-D60'};
+bison = {10000, 0.5*3600, 'BiSon64-ET-B'};
+nss = {12000, 0.5*3600, 'NSS CubeSat Sun Sensor'};
+
 
 % magnetometers
-
+tensortech = {15000, 1*3600, 'NSS Magnetometer'};
 
 [a, ~] = size(startrackers);
 names = cell(1, a);
@@ -39,6 +42,7 @@ hold on
 
 scatter(rocketlab{1}/1000, rocketlab{2}, sz, 'pentagram', 'red', 'filled')
 scatter(bison{1}/1000, bison{2}, sz, 'diamond', 'blue', 'filled')
+scatter(tensortech{1}/1000, tensortech{2}, sz, 'square', 'green', 'filled')
 scatter(ssoc{1}/1000, ssoc{2}, sz, 'diamond', 'blue', 'filled')
 
 for i = 2:a
@@ -49,13 +53,13 @@ for i = 2:a
 end
 hold off
 
-title('Cost VS Performance: Cross-Boresight')
+title('Cost VS Performance: Various Sensors')
 xlabel('Cost [in Thousands of $]')
-ylabel('Cross Boresight [arcsec]')
-set(gca,'yscale','log')
+ylabel('Accuracy [arcsec]')
+% set(gca,'yscale','log')
 grid on
 axis padded
-legend('Star Tracker', 'Sun Sensor')
+legend('Star Tracker', 'Sun Sensor', 'Magnetometer')
 
 figure
 hold on
@@ -67,7 +71,7 @@ for i = 1:a
 end
 hold off
 set(gca,'yscale','log')
-title('Cost VS Performance: Cross-Boresight')
+title('Cost VS Performance: Star Trackers')
 xlabel('Cost [in Thousands of $]')
 ylabel('Cross Boresight [arcsec]')
 grid on
